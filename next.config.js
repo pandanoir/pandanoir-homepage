f9d98c12c32e0a5188591a418c43d207e99e389b
pandanoir <moon_in_france@yahoo.co.jp>
2023-10-29 16:48:48 +0900

    redirect /prettierrc to gist

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/prettierrc',
+        destination:
+          'https://gist.githubusercontent.com/pandanoir/d133eeb8f36adf5d584e51644fd3bd33/raw/4b380279555ab8aba2e3a5d8286f900fddcbded3/.prettierrc',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
