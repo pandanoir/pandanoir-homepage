04ac325adb487c33899044cbd41d0226badbe379
pandanoir <pandanoir@ymail.ne.jp>
2025-04-12 10:23:36 +0900

    add profile section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { PropsWithChildren } from 'react';
+import { ComponentProps, PropsWithChildren } from 'react';
 import { fetchAllFeed } from './_utils/fetchAllFeed';
+import {
+  FaFilm,
+  FaMusic,
+  FaGamepad,
+  FaYoutube,
+} from 'react-icons/fa';
+import clsx from 'clsx';
 
 const Section = ({ children }: PropsWithChildren) => (
   <div className="flex flex-col bg-slate-700/40 p-3 h-max">{children}</div>
@@ -10,6 +17,9 @@ const Heading = ({ children }: PropsWithChildren) => (
   <h2 className="text-4xl font-bold border-b-2 border-gray-600 w-72 sm:w-80 md:w-96 lg:w-96 mb-3">
     {children}
   </h2>
+);
+const SubHeading = ({ children }: PropsWithChildren) => (
+  <h3 className="text-lg flex items-center gap-1">{children}</h3>
 );
 
 const HeroImage = () => (
@@ -28,6 +38,25 @@ const HeroContent = ({ children }: PropsWithChildren) => (
     {children}
   </div>
 );
+
+const ExternalLink = (props: ComponentProps<'a'>) => (
+  <a
+    {...props}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={clsx(props.className, 'hover:underline')}
+  />
+);
+const ExternalLinkList = (props: ComponentProps<'ul'>) => <ul {...props} />;
+const ExternalLinkListItem = ({
+  href,
+  children,
+}: PropsWithChildren<{ href: string }>) => (
+  <li>
+    <ExternalLink href={href}>{children}</ExternalLink>
+  </li>
+);
+
 const Home = async () => {
   const recentPost = (await fetchAllFeed())[0];
   return (
@@ -60,14 +89,78 @@ const Home = async () => {
           </Section>
 
           <Section>
-            <Heading>Works</Heading>
-            <a
-              href="https://hi-timer.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Hi-Timer
-            </a>
+            <Heading>Profile</Heading>
+            <div className="flex flex-col gap-3">
+              <div>
+                <SubHeading>
+                  <FaGamepad /> Hobbies
+                </SubHeading>
+                <ExternalLinkList className="pl-3">
+                  <li>映画</li>
+                  <li>合気道</li>
+                </ExternalLinkList>
+              </div>
+              <div>
+                <SubHeading>
+                  <FaFilm /> Top 5 Films
+                </SubHeading>
+                <ExternalLinkList className="pl-3">
+                  <ExternalLinkListItem href="https://www.imdb.com/title/tt0119472/">
+                    ノッキン・オン・ヘブンズ・ドア
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://www.imdb.com/title/tt0780536/">
+                    ヒットマンズレクイエム
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://www.imdb.com/title/tt7085058/">
+                    四月の永い夢
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://www.imdb.com/title/tt0110413/">
+                    レオン
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://www.imdb.com/title/tt0084602/">
+                    ロッキー3
+                  </ExternalLinkListItem>
+                </ExternalLinkList>
+              </div>
+              <div>
+                <SubHeading>
+                  <FaMusic /> Top 5 Artists
+                </SubHeading>
+                <ExternalLinkList className="pl-3">
+                  <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/012ldd58H1N5UZLxjMzV29?si=XJEJ9We_QXOwMXovcm285g">
+                    泉まくら
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/1bj13qFAwnyM3ILOiFnn1Y?si=lZNcP9ecSFG5S-ia0sJTyA">
+                    相対性理論
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/1mN9lPKzTRTOop4u7S1Uy9?si=CuSKWNGyR6e2syiQAfEH5g">
+                    iri
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/3rTxb36W3M1BCxx00iiwMU?si=LF8usd58SpKbbHRR7Otuww">
+                    空音
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/58oPVy7oihAEXE0Ott6JOf?si=u6_r0Q8pSHa8i9QxHKk7lg">
+                    Eve
+                  </ExternalLinkListItem>
+                </ExternalLinkList>
+              </div>
+              <div>
+                <SubHeading>
+                  <FaYoutube /> Top 3 Channels
+                </SubHeading>
+                <ExternalLinkList className="pl-3">
+                  <ExternalLinkListItem href="https://www.youtube.com/@omocorochannel">
+                    オモコロ
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://www.youtube.com/@TsukinoMito">
+                    月ノ美兎
+                  </ExternalLinkListItem>
+                  <ExternalLinkListItem href="https://www.youtube.com/@QuizKnock">
+                    QuizKnock
+                  </ExternalLinkListItem>
+                </ExternalLinkList>
+              </div>
+            </div>
           </Section>
 
           <Section>
