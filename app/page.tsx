cd66b9c97cd0c985ce0a663c27ab6adf74e01e8f
pandanoir <pandanoir@ymail.ne.jp>
2025-05-01 00:24:17 +0900

    change layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { ComponentProps, PropsWithChildren, ReactNode } from 'react';
 import { FaFilm, FaGamepad, FaMusic, FaYoutube } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
@@ -80,168 +81,198 @@ const IconLink = ({
 export default async function Home() {
   const recentPosts = (await fetchAllFeed()).slice(0, 3);
   return (
-    <div className="text-slate-300 px-8 flex flex-col justify-center gap-3 sm:gap-4">
-      <h1 className="flex-none text-5xl sm:text-6xl font-bold">pandanoir</h1>
-      ウェブフロントエンドエンジニア。ReactとTypeScriptに造詣が深い。
-      <ul className="flex gap-2">
-        <li>
-          <IconLink
-            href="https://x.com/le_panda_noir"
-            icon={<FaXTwitter size="1.3rem" />}
-            name="X"
+    <div className="text-slate-300 w-full max-w-[1680px] place-self-center flex lg:flex-row flex-col gap-3 px-2">
+      <div className="flex-2 max-h-screen flex items-center">
+        <div
+          className={clsx(
+            'grid justify-center max-h-screen h-max gap-y-3 gap-x-2 p-3',
+            `sm:[grid-template-areas:'icon_name''icon_description''links_links']`,
+            'sm:grid-cols-[max-content_1fr]',
+            `[grid-template-areas:'name''description''links']`,
+            'grid-rows-[repeat(3,min-content)]',
+            'grid-cols-1',
+          )}
+        >
+          <Image
+            src="/logo-black.png"
+            width="300"
+            height="300"
+            alt=""
+            className="sm:block hidden w-36 h-36 rounded-full [grid-area:icon]"
           />
-        </li>
-        <li>
-          <IconLink
-            href="https://pandanoir.info"
-            icon={
-              <HatenaBlogLogo
-                width="1.3rem"
-                height="1.3rem"
-                stroke="currentColor"
-                fill="currentColor"
-                viewBox="51 51 198 198"
+          <h1
+            className="text-5xl sm:text-6xl font-bold self-end [grid-area:name]"
+          >
+            pandanoir
+          </h1>
+          <p className="[grid-area:description]">
+            ウェブフロントエンドエンジニア。ReactとTypeScriptに造詣が深い。
+          </p>
+          <ul className="flex gap-2 [grid-area:links]">
+            <li>
+              <IconLink
+                href="https://x.com/le_panda_noir"
+                icon={<FaXTwitter size="1.3rem" />}
+                name="X"
               />
-            }
-            name="はてなブログ"
-          />
-        </li>
-        <li>
-          <IconLink
-            href="https://zenn.dev/pandanoir"
-            icon={<SiZenn size="1.3rem" />}
-            name="Zenn"
-          />
-        </li>
-        <li>
-          <IconLink
-            href="https://github.com/pandanoir"
-            icon={<FiGithub size="1.3rem" />}
-            name="GitHub"
-          />
-        </li>
-        <li>
-          <IconLink
-            href="https://www.wantedly.com/id/naoto_ikuno"
-            icon={<SiWantedly size="1.3rem" />}
-            name="Wantedly"
-          />
-        </li>
-        <li>
-          <IconLink
-            href="https://resume.pandanoir.net/"
-            icon={<BsFillPersonLinesFill size="1.3rem" />}
-            name="職務経歴書"
-          />
-        </li>
-      </ul>
-      <Section>
-        <Heading>Profile</Heading>
-        <div className="flex flex-col gap-3">
-          <div>
-            <SubHeading>
-              <FaGamepad /> Hobbies
-            </SubHeading>
-            <ExternalLinkList className="pl-3">
-              <li>映画</li>
-              <li>合気道</li>
-            </ExternalLinkList>
-          </div>
-          <div>
-            <SubHeading>
-              <FaFilm /> Top 5 Films
-            </SubHeading>
-            <ExternalLinkList className="pl-3">
-              <ExternalLinkListItem href="https://www.imdb.com/title/tt0119472/">
-                ノッキン・オン・ヘブンズ・ドア
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://www.imdb.com/title/tt0780536/">
-                ヒットマンズレクイエム
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://www.imdb.com/title/tt7085058/">
-                四月の永い夢
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://www.imdb.com/title/tt0110413/">
-                レオン
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://www.imdb.com/title/tt0084602/">
-                ロッキー3
-              </ExternalLinkListItem>
-            </ExternalLinkList>
-          </div>
-          <div>
-            <SubHeading>
-              <FaMusic /> Top 5 Artists
-            </SubHeading>
-            <ExternalLinkList className="pl-3">
-              <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/012ldd58H1N5UZLxjMzV29?si=XJEJ9We_QXOwMXovcm285g">
-                泉まくら
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/1bj13qFAwnyM3ILOiFnn1Y?si=lZNcP9ecSFG5S-ia0sJTyA">
-                相対性理論
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/1mN9lPKzTRTOop4u7S1Uy9?si=CuSKWNGyR6e2syiQAfEH5g">
-                iri
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/3rTxb36W3M1BCxx00iiwMU?si=LF8usd58SpKbbHRR7Otuww">
-                空音
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/58oPVy7oihAEXE0Ott6JOf?si=u6_r0Q8pSHa8i9QxHKk7lg">
-                Eve
-              </ExternalLinkListItem>
-            </ExternalLinkList>
-          </div>
-          <div>
-            <SubHeading>
-              <FaYoutube /> Top 3 Channels
-            </SubHeading>
-            <ExternalLinkList className="pl-3">
-              <ExternalLinkListItem href="https://www.youtube.com/@omocorochannel">
-                オモコロ
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://www.youtube.com/@TsukinoMito">
-                月ノ美兎
-              </ExternalLinkListItem>
-              <ExternalLinkListItem href="https://www.youtube.com/@QuizKnock">
-                QuizKnock
-              </ExternalLinkListItem>
-            </ExternalLinkList>
-          </div>
-        </div>
-      </Section>
-      <Section>
-        <Heading>Education</Heading>
-        <ul className="pl-3">
-          <li>2016-2020 東北大学 工学部 電気情報物理工学科 情報工学コース</li>
-          <li>2013-2016 新潟高校 普通科</li>
-        </ul>
-      </Section>
-      <Section>
-        <Heading>Recent posts</Heading>
-        <div className="flex gap-4 @container">
-          {recentPosts.map((post, i) => (
-            <a
-              key={post.title}
-              href={post.link}
-              rel="noreferrer"
-              className={clsx(
-                'w-96',
-                i === 2 ? 'hidden @7xl:block' : i === 1 && 'hidden @4xl:block',
-              )}
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={post.image}
-                className="w-96 aspect-ogp object-cover rounded"
-                alt=""
+            </li>
+            <li>
+              <IconLink
+                href="https://pandanoir.info"
+                icon={
+                  <HatenaBlogLogo
+                    width="1.3rem"
+                    height="1.3rem"
+                    stroke="currentColor"
+                    fill="currentColor"
+                    viewBox="51 51 198 198"
+                  />
+                }
+                name="はてなブログ"
               />
-              {post.title}
-            </a>
-          ))}
+            </li>
+            <li>
+              <IconLink
+                href="https://zenn.dev/pandanoir"
+                icon={<SiZenn size="1.3rem" />}
+                name="Zenn"
+              />
+            </li>
+            <li>
+              <IconLink
+                href="https://github.com/pandanoir"
+                icon={<FiGithub size="1.3rem" />}
+                name="GitHub"
+              />
+            </li>
+            <li>
+              <IconLink
+                href="https://www.wantedly.com/id/naoto_ikuno"
+                icon={<SiWantedly size="1.3rem" />}
+                name="Wantedly"
+              />
+            </li>
+            <li>
+              <IconLink
+                href="https://resume.pandanoir.net/"
+                icon={<BsFillPersonLinesFill size="1.3rem" />}
+                name="職務経歴書"
+              />
+            </li>
+          </ul>
         </div>
-        <br />
-        <Link href="/posts">read more</Link>
-      </Section>
+      </div>
+      <div className="flex-3 flex flex-col gap-3">
+        <Section>
+          <Heading>Profile</Heading>
+          <div className="flex flex-col gap-3">
+            <div>
+              <SubHeading>
+                <FaGamepad /> Hobbies
+              </SubHeading>
+              <ExternalLinkList className="pl-3">
+                <li>映画</li>
+                <li>合気道</li>
+              </ExternalLinkList>
+            </div>
+            <div>
+              <SubHeading>
+                <FaFilm /> Top 5 Films
+              </SubHeading>
+              <ExternalLinkList className="pl-3">
+                <ExternalLinkListItem href="https://www.imdb.com/title/tt0119472/">
+                  ノッキン・オン・ヘブンズ・ドア
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://www.imdb.com/title/tt0780536/">
+                  ヒットマンズレクイエム
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://www.imdb.com/title/tt7085058/">
+                  四月の永い夢
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://www.imdb.com/title/tt0110413/">
+                  レオン
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://www.imdb.com/title/tt0084602/">
+                  ロッキー3
+                </ExternalLinkListItem>
+              </ExternalLinkList>
+            </div>
+            <div>
+              <SubHeading>
+                <FaMusic /> Top 5 Artists
+              </SubHeading>
+              <ExternalLinkList className="pl-3">
+                <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/012ldd58H1N5UZLxjMzV29?si=XJEJ9We_QXOwMXovcm285g">
+                  泉まくら
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/1bj13qFAwnyM3ILOiFnn1Y?si=lZNcP9ecSFG5S-ia0sJTyA">
+                  相対性理論
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/1mN9lPKzTRTOop4u7S1Uy9?si=CuSKWNGyR6e2syiQAfEH5g">
+                  iri
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/3rTxb36W3M1BCxx00iiwMU?si=LF8usd58SpKbbHRR7Otuww">
+                  空音
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://open.spotify.com/intl-ja/artist/58oPVy7oihAEXE0Ott6JOf?si=u6_r0Q8pSHa8i9QxHKk7lg">
+                  Eve
+                </ExternalLinkListItem>
+              </ExternalLinkList>
+            </div>
+            <div>
+              <SubHeading>
+                <FaYoutube /> Top 3 Channels
+              </SubHeading>
+              <ExternalLinkList className="pl-3">
+                <ExternalLinkListItem href="https://www.youtube.com/@omocorochannel">
+                  オモコロ
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://www.youtube.com/@TsukinoMito">
+                  月ノ美兎
+                </ExternalLinkListItem>
+                <ExternalLinkListItem href="https://www.youtube.com/@QuizKnock">
+                  QuizKnock
+                </ExternalLinkListItem>
+              </ExternalLinkList>
+            </div>
+          </div>
+        </Section>
+        <Section>
+          <Heading>Education</Heading>
+          <ul className="pl-3">
+            <li>2016-2020 東北大学 工学部 電気情報物理工学科 情報工学コース</li>
+            <li>2013-2016 新潟高校 普通科</li>
+          </ul>
+        </Section>
+        <Section>
+          <Heading>Recent posts</Heading>
+          <div className="flex gap-4 @container">
+            {recentPosts.map((post, i) => (
+              <a
+                key={post.title}
+                href={post.link}
+                rel="noreferrer"
+                className={clsx(
+                  'w-72',
+                  i === 2
+                    ? 'hidden @4xl:block'
+                    : i === 1 && 'hidden @2xl:block',
+                )}
+              >
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={post.image}
+                  className="w-72 aspect-ogp object-cover rounded"
+                  alt=""
+                />
+                {post.title}
+              </a>
+            ))}
+          </div>
+          <br />
+          <Link href="/posts">read more</Link>
+        </Section>
+      </div>
     </div>
   );
 }
