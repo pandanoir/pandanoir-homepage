2af7dd3543943110f943b9bf2dfed2e967571dd0
pandanoir <pandanoir@ymail.ne.jp>
2025-05-06 00:12:52 +0900

    save locale setting in cookie

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const locales = ['en-us', 'en', 'ja'] as const;
+const secondsPerYear = 60 * 60 * 24 * 365;
 
+// ロケール関連の処理をしている(リダイレクトする、cookieにロケールをセットする)
 export function middleware(request: NextRequest) {
   const { pathname, origin } = request.nextUrl;
   const localeInPathname = locales.find(
@@ -10,26 +12,39 @@ export function middleware(request: NextRequest) {
       pathname.toLowerCase() === `/${locale}`,
   );
 
-  // ja locale はリダイレクトする
   if (localeInPathname === 'ja') {
-    return NextResponse.redirect(
-      new URL(pathname.replace(/^\/ja(\/|$)/, '/'), origin),
+    // ja locale は /(ロケールなし) にリダイレクトする
+    const response = NextResponse.redirect(
+      new URL(pathname.replace(/^\/ja(\/?)/, '$1'), origin),
     );
+    response.cookies.set('locale', 'ja', { path: '/', maxAge: secondsPerYear });
+    return response;
   }
-  // en locale はそのまま返す
-  if (localeInPathname === 'en') {
-    return NextResponse.next();
-  }
-  // en-us locale は en に統合
   if (localeInPathname === 'en-us') {
+    // en-us locale は en に統合する
     return NextResponse.redirect(
       new URL(pathname.replace(/^\/en-us/i, '/en'), origin),
     );
   }
+  if (localeInPathname === 'en') {
+    // en locale はそのまま返す
+    const response = NextResponse.next();
+    response.cookies.set('locale', 'en', { path: '/', maxAge: secondsPerYear });
+    return response;
+  }
+  localeInPathname satisfies undefined;
 
-  // デフォではja localeと解釈する
-  const redirectUrl = new URL(`/ja${pathname}`, origin);
-  return NextResponse.rewrite(redirectUrl);
+  // localeがURLで指定されてない場合、適切なロケールへリダイレクトする
+  // (localeをjaに変更する場合は/jaに明示的にアクセスする方針にしたので、このケースにおいて設定の変更について考えなくて良い)
+  const preferredLocale =
+    request.headers.get('accept-language')?.split(',')[0].split('-')[0] ?? 'ja';
+  const locale = request.cookies.get('locale')?.value ?? preferredLocale;
+
+  if (locale !== 'ja') {
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, origin));
+  }
+  // jaのときはrewriteを使って、内部的には/ja/でアクセスしたことにする (lang paramを受け取るため)
+  return NextResponse.rewrite(new URL(`/ja${pathname}`, origin));
 }
 
 export const config = {
