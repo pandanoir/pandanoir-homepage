73c9eb934c829c002ae8f50807cb57c562f8c361
pandanoir <pandanoir@ymail.ne.jp>
2025-05-06 17:21:13 +0900

    fix margin and heading text of posts page

--- app/[lang]/posts/page.tsx
+++ app/[lang]/posts/page.tsx
@@ -10,11 +10,11 @@ export default async function PostPage({
 }) {
   const { lang } = ParamsSchema.parse(await params);
   return (
-    <div className="text-slate-300 px-8 flex flex-col justify-center gap-3 sm:gap-4">
+    <div className="text-slate-300 px-8 mb-8 flex flex-col justify-center gap-3 sm:gap-4">
       <h1 className="flex-none text-5xl sm:text-6xl font-bold">
         <Link href={`/${lang}`}>pandanoir</Link>
       </h1>
-      <h2 className="text-4xl">recent posts</h2>
+      <h2 className="text-4xl">Recent posts</h2>
       <ArticleList posts={await fetchAllFeed()} />
     </div>
   );
