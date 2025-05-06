694310f78fdce540f6f9a274d79ecddba04c22bf
pandanoir <pandanoir@ymail.ne.jp>
2025-05-06 12:56:47 +0900

    change design of posts page - remove ogp image - assign background color by source - add year-by-year separators

--- app/[lang]/posts/ArticleList.tsx
+++ app/[lang]/posts/ArticleList.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { HatenaBlogLogo } from '@/app/_components/HatenaBlogLogo';
+import clsx from 'clsx';
 import { useState } from 'react';
 import { FaBuilding } from 'react-icons/fa6';
 import { SiZenn } from 'react-icons/si';
@@ -14,62 +15,72 @@ type Article = {
 };
 export const ArticleList = ({ posts }: { posts: Article[] }) => {
   const [hasClickedReadMore, setHasClickedReadMore] = useState(false);
+  const slicedPosts = hasClickedReadMore ? posts : posts.slice(0, 23);
 
   return (
-    <ul className="flex flex-wrap gap-x-4 gap-y-8 justify-center">
-      {(hasClickedReadMore ? posts : posts.slice(0, 23)).map((x) => (
-        <li
-          key={x.link}
-          className="w-128 h-min bg-gray-800 rounded shadow-sm overflow-hidden"
-        >
-          <a
-            href={x.link}
-            className="inline-block"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {x.image && (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                src={x.image}
-                alt=""
-                className="w-128 aspect-ogp object-cover"
-              />
-            )}
-            <div className="px-2 py-1">
-              <span suppressHydrationWarning={true} className="font-bold">
-                <span className="inline-block align-middle mr-1">
+    <>
+      <ul className="flex flex-col gap-y-2 justify-center">
+        {slicedPosts.map((x, i) => (
+          <>
+            <li key={x.link}>
+              <a
+                href={x.link}
+                className={clsx(
+                  'flex flex-col px-2 py-1 max-w-256 rounded shadow-sm overflow-hidden group',
                   {
-                    {
-                      'hatena blog': <HatenaBlogLogo width="1rem" />,
-                      zenn: <SiZenn size="1rem" />,
-                      company: <FaBuilding size="1rem" />,
-                    }[x.source]
-                  }
-                </span>
-                {x.title}
-              </span>
-              <br />
-              <time
-                dateTime={x.pubDate.toLocaleString('ja-JP')}
-                className="text-sm"
+                    zenn: 'bg-[#3EA8FF] text-gray-900',
+                    'hatena blog': 'bg-gray-300 text-gray-800',
+                    company: 'bg-emerald-300 text-gray-800',
+                  }[x.source],
+                )}
+                target="_blank"
+                rel="noreferrer"
               >
-                {x.pubDate.toLocaleDateString('ja-JP')}
-              </time>
-            </div>
-          </a>
-        </li>
-      ))}
+                <span
+                  suppressHydrationWarning={true}
+                  className="font-bold group-hover:underline"
+                >
+                  <span className="pl-4 mr-1 relative">
+                    <span className="absolute left-0 top-1/2 -mt-2">
+                      {
+                        {
+                          'hatena blog': <HatenaBlogLogo width="1rem" />,
+                          zenn: <SiZenn size="1rem" />,
+                          company: <FaBuilding size="1rem" />,
+                        }[x.source]
+                      }
+                    </span>
+                  </span>
+                  {x.title}
+                </span>
+                <time
+                  dateTime={x.pubDate.toLocaleString('ja-JP')}
+                  className="text-sm"
+                >
+                  {x.pubDate.toLocaleDateString('ja-JP')}
+                </time>
+              </a>
+            </li>
+            {i + 1 < slicedPosts.length &&
+              x.pubDate.getFullYear() !==
+                slicedPosts[i + 1].pubDate.getFullYear() && (
+                <div className="max-w-256 grid grid-cols-[1fr_max-content_1fr] items-center gap-2">
+                  <div className="bg-gray-300 h-[1px]" />
+                  <div>{x.pubDate.getFullYear()}年</div>
+                  <div className="bg-gray-300 h-[1px]" />
+                </div>
+              )}
+          </>
+        ))}
+      </ul>
       {!hasClickedReadMore && (
-        <li className="w-128 h-min bg-gray-800 rounded shadow-sm overflow-hidden">
-          <button
-            className="w-128 h-64"
-            onClick={() => setHasClickedReadMore(true)}
-          >
-            readmore≫
-          </button>
-        </li>
+        <button
+          className="border border-gray-500 rounded px-4 py-2 bg-gray-800 w-max"
+          onClick={() => setHasClickedReadMore(true)}
+        >
+          readmore≫
+        </button>
       )}
-    </ul>
+    </>
   );
 };
