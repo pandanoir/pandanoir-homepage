96d5fe22e1be7f960a94369daaa5d1b7fd598cf1
pandanoir <pandanoir@ymail.ne.jp>
2025-05-06 17:57:23 +0900

    move year separator from ul

--- app/[lang]/posts/ArticleList.tsx
+++ app/[lang]/posts/ArticleList.tsx
@@ -16,63 +16,66 @@ type Article = {
 export const ArticleList = ({ posts }: { posts: Article[] }) => {
   const [hasClickedReadMore, setHasClickedReadMore] = useState(false);
   const slicedPosts = hasClickedReadMore ? posts : posts.slice(0, 23);
+  const postsByYear = [
+    ...Map.groupBy(slicedPosts, (post) => post.pubDate.getFullYear()).entries(),
+  ].sort((a, b) => Number(b[0]) - Number(a[0]));
 
   return (
     <>
-      <ul className="flex flex-col gap-y-2 justify-center">
-        {slicedPosts.map((x, i) => (
-          <>
-            <li key={x.link}>
-              <a
-                href={x.link}
-                className={clsx(
-                  'flex flex-col px-2 py-1 max-w-256 rounded shadow-sm overflow-hidden group',
-                  {
-                    zenn: 'bg-[#3EA8FF] text-gray-900',
-                    'hatena blog': 'bg-gray-300 text-gray-800',
-                    company: 'bg-emerald-300 text-gray-800',
-                  }[x.source],
-                )}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span
-                  suppressHydrationWarning={true}
-                  className="font-bold group-hover:underline"
+      {postsByYear.map(([year, posts], i) => (
+        <>
+          <ul className="flex flex-col gap-y-2 justify-center">
+            {posts.map((x) => (
+              <li key={x.link}>
+                <a
+                  href={x.link}
+                  className={clsx(
+                    'flex flex-col px-2 py-1 max-w-256 rounded shadow-sm overflow-hidden group',
+                    {
+                      zenn: 'bg-[#3EA8FF] text-gray-900',
+                      'hatena blog': 'bg-gray-300 text-gray-800',
+                      company: 'bg-emerald-300 text-gray-800',
+                    }[x.source],
+                  )}
+                  target="_blank"
+                  rel="noreferrer"
                 >
-                  <span className="pl-4 mr-1 relative">
-                    <span className="absolute left-0 top-1/2 -mt-2">
-                      {
+                  <span
+                    suppressHydrationWarning={true}
+                    className="font-bold group-hover:underline"
+                  >
+                    <span className="pl-4 mr-1 relative">
+                      <span className="absolute left-0 top-1/2 -mt-2">
                         {
-                          'hatena blog': <HatenaBlogLogo width="1rem" />,
-                          zenn: <SiZenn size="1rem" />,
-                          company: <FaBuilding size="1rem" />,
-                        }[x.source]
-                      }
+                          {
+                            'hatena blog': <HatenaBlogLogo width="1rem" />,
+                            zenn: <SiZenn size="1rem" />,
+                            company: <FaBuilding size="1rem" />,
+                          }[x.source]
+                        }
+                      </span>
                     </span>
+                    {x.title}
                   </span>
-                  {x.title}
-                </span>
-                <time
-                  dateTime={x.pubDate.toLocaleString('ja-JP')}
-                  className="text-sm"
-                >
-                  {x.pubDate.toLocaleDateString('ja-JP')}
-                </time>
-              </a>
-            </li>
-            {i + 1 < slicedPosts.length &&
-              x.pubDate.getFullYear() !==
-                slicedPosts[i + 1].pubDate.getFullYear() && (
-                <div className="max-w-256 grid grid-cols-[1fr_max-content_1fr] items-center gap-2">
-                  <div className="bg-gray-300 h-[1px]" />
-                  <div>{x.pubDate.getFullYear()}年</div>
-                  <div className="bg-gray-300 h-[1px]" />
-                </div>
-              )}
-          </>
-        ))}
-      </ul>
+                  <time
+                    dateTime={x.pubDate.toLocaleString('ja-JP')}
+                    className="text-sm"
+                  >
+                    {x.pubDate.toLocaleDateString('ja-JP')}
+                  </time>
+                </a>
+              </li>
+            ))}
+          </ul>
+          {i !== Object.keys(postsByYear).length - 1 && (
+            <div className="max-w-256 grid grid-cols-[1fr_max-content_1fr] items-center gap-2">
+              <div className="bg-gray-300 h-[1px]" />
+              <div>{year}年</div>
+              <div className="bg-gray-300 h-[1px]" />
+            </div>
+          )}
+        </>
+      ))}
       {!hasClickedReadMore && (
         <button
           className="border border-gray-500 rounded px-4 py-2 bg-gray-800 w-max"
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import 'core-js/features/map/group-by';
+import { PropsWithChildren } from 'react';
 
 export const metadata: Metadata = {
   title: 'pandanoir',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return <>{children}</>;
 }
