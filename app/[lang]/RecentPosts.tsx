ccf1a72b49fb9a028eb8a4a55f84e3c9901fef5c
pandanoir <pandanoir@ymail.ne.jp>
2025-05-15 16:10:27 +0900

    add pubDate to RecentPosts

--- app/[lang]/RecentPosts.tsx
+++ app/[lang]/RecentPosts.tsx
@@ -3,39 +3,48 @@ import { HatenaBlogLogo } from '../_components/HatenaBlogLogo';
 import { fetchAllFeed } from '../_utils/fetchAllFeed';
 import { FaBuilding } from 'react-icons/fa6';
 import clsx from 'clsx';
+import { FaCalendar } from 'react-icons/fa';
 
 export async function RecentPosts() {
   await new Promise((r) => setTimeout(r, 500));
   const recentPosts = (await fetchAllFeed()).slice(0, 5);
   return (
-    <ul className="flex flex-col gap-1">
+    <ul className="grid grid-cols-1 sm:grid-cols-[1fr_max-content] gap-x-2 gap-y-0 sm:gap-y-1">
       {recentPosts.map((post) => (
-        <li key={post.title}>
+        <li key={post.title} className="contents">
           <a
             href={post.link}
-            className="hover:underline rounded-sm px-1 py-0.5"
+            className="contents hover:underline rounded-sm px-1 py-0.5"
           >
-            <span className="pl-4 mr-1 relative">
-              <span
-                className={clsx(
-                  'absolute p-0.5 -left-0.5 top-1/2 -mt-2.5 rounded-full',
+            <span>
+              <span className="pl-4 mr-1 relative">
+                <span
+                  className={clsx(
+                    'absolute p-0.5 -left-0.5 top-1/2 -mt-2.5 rounded-full',
+                    {
+                      zenn: 'bg-[#3EA8FF] text-gray-900',
+                      'hatena blog': 'bg-gray-300 text-gray-800',
+                      company: 'bg-emerald-300 text-gray-800',
+                    }[post.source],
+                  )}
+                >
                   {
-                    zenn: 'bg-[#3EA8FF] text-gray-900',
-                    'hatena blog': 'bg-gray-300 text-gray-800',
-                    company: 'bg-emerald-300 text-gray-800',
-                  }[post.source],
-                )}
-              >
-                {
-                  {
-                    'hatena blog': <HatenaBlogLogo width="1rem" />,
-                    zenn: <SiZenn size="1rem" />,
-                    company: <FaBuilding size="1rem" />,
-                  }[post.source]
-                }
+                    {
+                      'hatena blog': <HatenaBlogLogo width="1rem" />,
+                      zenn: <SiZenn size="1rem" />,
+                      company: <FaBuilding size="1rem" />,
+                    }[post.source]
+                  }
+                </span>
+              </span>
+              {post.title}
+            </span>
+            <span className="text-gray-400 text-sm pb-2 sm:pb-0">
+              <span className="inline-flex items-center">
+                <FaCalendar className="inline" />
+                {post.pubDate.toLocaleDateString('ja-JP')}
               </span>
             </span>
-            {post.title}
           </a>
         </li>
       ))}
